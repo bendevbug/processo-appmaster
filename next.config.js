2f684c86362bd925f9f3b3346ac5f9d652ab21c2
unknown <maysaraquel.ia@gmail.com>
2023-06-25 20:30:55 -0300

    Projeto realizado

--- next.config.js
+++ next.config.js
@@ -3,24 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.module\.css$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true,
-            localIdentName: '[name]__[local]--[hash:base64:5]',
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
-};
-
 module.exports = nextConfig
